8fdd3e2fb5565f89accc445920a9490e3e24558d
Rei-L0 <qkrdusgn00@naver.com>
2023-08-07 10:55:42 +0900

    frontend react 추가

---
+++ reactchat/src/App.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter, Route, RouterProvider, createRoutesFromElements } from "react-router-dom";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route>
+            <Route path="/" element={<Home />} />
+        </Route>
+    )
+);
+
+const App = () => {
+    return <RouterProvider router={router} />;
+};
+
+export default App;
